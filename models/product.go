6ca509b42dcc1080e0f01d4eb6ca08dad711dca9
A.Mohan <mohan6121@email.com>
2025-05-26 17:53:22 +0530

    mohan

--- models/product.go
+++ models/product.go
@@ -1,11 +1,9 @@
 package models
 
 type Product struct {
-	ID         uint     `gorm:"primaryKey" json:"id"`
-	Name       string   `gorm:"not null" json:"name"`
-	Price      float64  `json:"price"`
-	Stock      int      `json:"stock"`
-	ImageURL   string   `gorm:"not null" json:"image_url"`
-	CategoryID uint     `json:"category_id"`
-	Category   Category `gorm:"foreignKey:CategoryID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"category,omitempty"`
+    ID         uint    `gorm:"primaryKey"`
+    Name       string  `gorm:"not null"`
+    Price      float64
+    Stock      int
+    CategoryID uint
 }
