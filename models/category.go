72297295cc15164f623212aba9e3bcd27fb75e6f
A.Mohan <mohan6121@email.com>
2025-05-26 18:22:39 +0530

    mohan

--- models/category.go
+++ models/category.go
@@ -2,7 +2,7 @@ package models
 
 type Category struct {
 	ID       uint     `gorm:"primaryKey" json:"id"`
-	Name     string   `gorm:"not null" json:"name"`
-	ImageURL string   `gorm:"not null" json:"image_url"`
-	Products []Product `json:"products,omitempty"`
+	Name     string   `json:"name"`
+	ImageURL string   `json:"image_url"`
+	Products []Product `gorm:"foreignKey:CategoryID" json:"products"` // Optional: to preload all products in a category
 }
